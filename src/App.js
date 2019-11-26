7a53e841d4d47f9200fa4d1efd8a130c69a3812a
Eduardo Ramos <eduardorxs@gmail.com>
2019-11-26 19:40:22 -0300

    Estruturando componentes

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import React, { useEffect } from "react";
+import SearchBar from "./components/layout/SearchBar";
+import Logs from "./components/logs/Logs";
 
 import "materialize-css/dist/css/materialize.min.css";
 import M from "materialize-css/dist/js/materialize.min.js";
@@ -6,10 +8,17 @@ import "./App.css";
 
 const App = () => {
 	useEffect(() => {
-		// Inicializa Materialize JS
+		// Inicializa Materialize JS, permitindo usar componentes javascript como, por exemplo, modais
 		M.AutoInit();
 	});
-	return <div className="App">Meu app</div>;
+	return (
+		<div className="App">
+			<SearchBar />
+			<div className="container">
+				<Logs />
+			</div>
+		</div>
+	);
 };
 
 export default App;
